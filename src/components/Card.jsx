70435a69412b7f052933a14d88f8f4a8ec37fc04
vivizuma <vito.manzullo@gmail.com>
2024-04-12 14:41:24 +0100

    commit for vercel hosting

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,15 @@
-function Card() {
+import "./Card.css";
+// i can pass image urls to the img src
+
+function Card(props) {
   return (
     <>
-      <div>
+      <div className="card">
+        {props.img}
         image here
         <img src="" alt="" />
         <p>
+          {props.name}
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam harum
           porro deserunt corporis necessitatibus sint quo minus iste vitae
           explicabo.
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,9 +1,29 @@
 import { useState, useEffect } from "react";
 import Card from "./Card";
+
 function Gameboard() {
+  const [round, setRound] = useState(1);
+  const [loading, isLoading] = useState(true);
+
+  let array = [1, 2, 3, 4, 5, 6];
+  // on mount and when round count increases.. fetch new cards
+  useEffect(() => {
+    
+  }, [round]);
+  //if loading state is true - render skeleton
+  //if loading is false, render real cards
+  // fetch 6 images from an api at random(ish)
+  // store as array of 6
+  // create an object for each card, with a boolean isClicked
+
+  // randomise order
+  // pass index as a prop to a card to display image
   return (
     <>
       <div>This is a card</div>
+      <Card name={array[0]} />
+      <Card img="https://picsum.photos/id/237/200/300" />
+      <Card name={name} />
     </>
   );
 }
